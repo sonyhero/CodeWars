5faa849b5a3c4bdf0077bdf26df5da99046005af
sonyhero notebook <27anton06@gmail.com>
2023-02-08 20:11:52 +0300

    Replace With Alphabet Position - part 1

--- 6kyu/Replace With Alphabet Position.js
+++ 6kyu/Replace With Alphabet Position.js
@@ -13,7 +13,13 @@ Example
 alphabetPosition("The sunset sets at twelve o' clock.")
 Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
 */
+console.log("The sunset sets at twelve o' clock.".toLowerCase()
+.replace(/[^a-z]/g, '')
+.split('')
+.map(x=>x.codePointAt(0)-97)
+.join(' ')
+)
 
 function alphabetPosition(text) {
-  
+   return text.toLowerCase()
 }
