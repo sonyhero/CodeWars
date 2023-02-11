0c9de7b1ab9eb6b2fd9ac1cc7f535e68041b000e
sonyhero <27anton06@gmail.com>
2023-02-11 16:43:46 +0300

    The 'if' function - ref

--- 8kyu/The 'if' function.js
+++ 8kyu/The 'if' function.js
@@ -8,6 +8,4 @@ _if(true, function(){console.log("True")}, function(){console.log("false")})
 // Logs 'True' to the console.
 */
 
-function _if(bool, func1, func2) {
-    return bool ? func1() : func2();
-}
+const _if = (bool, func1, func2) => bool ? func1() : func2();
