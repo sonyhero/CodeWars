2a9a6b3d842c1037f37695ab16476209814b8c71
sonyhero notebook <27anton06@gmail.com>
2023-02-13 19:21:40 +0300

    Enumerable Magic #25 - Take the First N Elements

---
+++ 8kyu/Enumerable Magic 25 - Take the First N Elements.js
@@ -0,0 +1,10 @@
+/*
+Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+*/
+
+function take(arr, n) {
+    // Your code here
+  }
+
+
+console.log(take([0, 1, 2, 3, 5, 8, 13], 3));
