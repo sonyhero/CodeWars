34ba44459bd5aa68cbca6ebb6cb8b25eb6713c77
sonyhero notebook <27anton06@gmail.com>
2023-02-13 19:19:57 +0300

    Predict your age! - done

--- 7kyu/Predict your age!.js
+++ 7kyu/Predict your age!.js
@@ -13,7 +13,8 @@ predictAge(65, 60, 75, 55, 60, 63, 64, 45) === 86
 */
 
 function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
-    // your code
+    arr = [age1,age2,age3,age4,age5,age6,age7,age8];
+    return Math.floor(Math.sqrt(arr.map(x=>x*x).reduce((a,b)=>a+b))/2) 
   }
 
 console.log(predictAge(65,60,75,55,60,63,64,45));
