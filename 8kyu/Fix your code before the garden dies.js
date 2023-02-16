096b4380d0a402084d3db621b87820d8c5649912
sonyhero notebook <27anton06@gmail.com>
2023-02-16 21:25:19 +0300

    Fix your code before the garden dies - refactoring

--- 8kyu/Fix your code before the garden dies.js
+++ 8kyu/Fix your code before the garden dies.js
@@ -5,7 +5,5 @@ Your task is to debug the code before your plants die!
 */
 
 function rainAmount(mm) {
-  if (mm < 40) {
-    return `You need to give your plant ${40 - mm}mm of water`;
-  } else return "Your plant has had more than enough water for today!";
+  return (mm < 40) ? `You need to give your plant ${40 - mm}mm of water` : "Your plant has had more than enough water for today!";
 }
