88fa727c2f3798f58367e54abf97430eb063c591
sonyhero notebook <27anton06@gmail.com>
2023-02-16 21:08:26 +0300

    CSV representation of array - done

--- 8kyu/CSV representation of array.js
+++ 8kyu/CSV representation of array.js
@@ -3,10 +3,14 @@ CSV representation of array
 */
 
 function toCsvText(array) {
-    return 
- }
+  return array.map((x) => x.join(",")).join("\n");
+}
 
-console.log(toCsvText([[ 0, 1, 2, 3, 4 ],
-    [ 10,11,12,13,14 ],
-    [ 20,21,22,23,24 ],
-    [ 30,31,32,33,34 ]] ))
+console.log(
+  toCsvText([
+    [0, 1, 2, 3, 4],
+    [10, 11, 12, 13, 14],
+    [20, 21, 22, 23, 24],
+    [30, 31, 32, 33, 34],
+  ])
+);
