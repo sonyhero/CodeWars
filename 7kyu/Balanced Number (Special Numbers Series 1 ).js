bb1c22b2c1cde76528e7040107bcdab973c0afd4
sonyhero <27anton06@gmail.com>
2023-02-17 16:27:02 +0300

    Balanced Number (Special Numbers Series #1 )

---
+++ 7kyu/Balanced Number (Special Numbers Series 1 ).js
@@ -0,0 +1,25 @@
+/*
+A balanced number is a number where the sum of digits to the left of the middle digit(s) and the sum of digits to the right of the middle digit(s) are equal.
+
+If the number has an odd number of digits, then there is only one middle digit. (For example, 92645 has one middle digit, 6.) Otherwise, there are two middle digits. (For example, the middle digits of 1301 are 3 and 0.)
+
+The middle digit(s) should not be considered when determining whether a number is balanced or not, e.g. 413023 is a balanced number because the left sum and right sum are both 5.
+
+The task
+Given a number, find if it is balanced, and return the string "Balanced" or "Not Balanced" accordingly. The passed number will always be positive.
+
+295591 ==> return "Not Balanced"
+Explanation:
+middle digit(s): 55
+sum of all digits to the left of the middle digit(s) -> 11
+sum of all digits to the right of the middle digit(s) -> 10
+11 and 10 are not equal, so it's not balanced.
+*/
+
+function balancedNum(number)
+{
+    return "Do your magic!"
+}
+
+console.log();
+
