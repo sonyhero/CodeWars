2bb46cc5db25bb903ae49a5652cf6f328f0b3b96
sonyhero <27anton06@gmail.com>
2023-02-19 17:36:55 +0300

    Categorize New Member - done

--- 7kyu/Categorize New Member.js
+++ 7kyu/Categorize New Member.js
@@ -1,7 +1,9 @@
 /*
-The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+The Western Suburbs Croquet Club has two categories of membership, Senior and Open. 
+They would like your help with an application form that will tell prospective members which category they will be placed.
 
-To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+To be a senior, a member must be at least 55 years old and have a handicap greater than 7. 
+In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
 
 Input
 Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
@@ -15,7 +17,12 @@ output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
 */
 
 function openOrSenior(data){
-    // ...
+    return data.map(x=>{
+      if(x[0]>=55 & x[1]>7)  {
+        x= "Senior"
+      } else x = "Open";
+      return x
+    })
   }
 
 console.log(openOrSenior([[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]));
