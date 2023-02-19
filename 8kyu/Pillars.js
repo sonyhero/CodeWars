1584c4bc028694816409fb43f44bf350dccfd7ed
sonyhero <27anton06@gmail.com>
2023-02-19 16:53:05 +0300

    Pillars - done

--- 8kyu/Pillars.js
+++ 8kyu/Pillars.js
@@ -9,6 +9,7 @@ pillars(2, 20, 25) --> 2000;
 */
 
 function pillars(numPill, dist, width) {
+    return (numPill<2) ? 0 : (numPill-1)*dist*100+(numPill-2)*width;
   }
 
 console.log(pillars(2, 20, 25));
