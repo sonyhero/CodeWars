1b22cc37c25b29f1f2ed2dc1afb8e03240dfef25
sonyhero notebook <27anton06@gmail.com>
2023-02-20 21:02:34 +0300

    Convert an array of strings to... - refactoring

--- 8kyu/Convert an array of strings to array of numbers.js
+++ 8kyu/Convert an array of strings to array of numbers.js
@@ -11,8 +11,6 @@ ie:["1", "2", "3"] to [1, 2, 3]
 Note that you can receive floats as well.
 */
 
-function toNumberArray(stringarray) {
-    return stringarray.map(x=>+x);
-}
+const toNumberArray = (str) => str.map(x=>+x);
 
 console.log(toNumberArray(["1.1", "2.2", "3.3"]));
