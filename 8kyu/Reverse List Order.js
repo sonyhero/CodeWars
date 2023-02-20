3d456c17dc841e0abc803d5a537fb1f3e6fd9a8c
sonyhero notebook <27anton06@gmail.com>
2023-02-20 19:41:49 +0300

    Reverse List Order - done

--- 8kyu/Reverse List Order.js
+++ 8kyu/Reverse List Order.js
@@ -6,6 +6,8 @@ Examples (Input -> Output)
 * [9, 2, 0, 7]  -> [7, 0, 2, 9]
 */
 
-function reverseList(list){
-    
-}
+function reverseList(list) {
+    return list.reverse();
+   }
+
+console.log(reverseList([1,2,3,4]))
