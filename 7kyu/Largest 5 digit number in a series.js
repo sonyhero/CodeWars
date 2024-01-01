0100a57dbc47475e5629141411c764e1b5219181
sonyhero <27anton06@gmail.com>
2024-01-01 18:59:56 +0300

    Largest 5 digit number in a series

---
+++ 7kyu/Largest 5 digit number in a series.js
@@ -0,0 +1,24 @@
+// In the following 6 digit number:
+//
+//     283910
+// 91 is the greatest sequence of 2 consecutive digits.
+//
+//     In the following 10 digit number:
+//
+//     1234567890
+// 67890 is the greatest sequence of 5 consecutive digits.
+//
+//     Complete the solution so that it returns the greatest sequence of five consecutive digits found within the number given. The number will be passed in as a string of only digits. It should return a five digit integer. The number passed may be as large as 1000 digits.
+//
+//     Adapted from ProjectEuler.net
+
+const solution = (digits) => {
+    let maxNumber = Number(digits.slice(0, 5))
+    for (let i = 1; i < digits.length; i++) {
+        let currentNumber = Number(digits.slice(i, i + 5))
+        if (currentNumber > maxNumber) {
+            maxNumber = currentNumber
+        }
+    }
+    return maxNumber
+}
